71ac9c345fda40609306332438d372f9c9377735
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-17 21:10:57 +0200

    learn component templates & interaction

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, signal, Signal } from '@angular/core';
+// this is where we define behavior
 @Component({
   selector: 'app-hello', // basically a tag
   imports: [],
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './hello.css',
 })
 export class Hello {
-  
+  protected title:string = 'Welcome to Modern Angular!'
+
+  protected age = signal(0);
+
+  increment(): void {
+    this.age.update(value => value + 1)
+  }
+
+  protected isDisabled = false;
+  onClick(): void {
+    console.log('Button clicked');
+    this.isDisabled = !this.isDisabled;
+  }
 }
