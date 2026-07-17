509c90d08711d1e52221890acab5eef68e7288e6
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-17 18:23:50 +0200

    get to grips with basic angular

---
+++ src/app/hello/hello.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello', // basically a tag
+  imports: [],
+  templateUrl: './hello.html', // reference to html file
+  styleUrl: './hello.css',
+})
+export class Hello {
+  
+}
