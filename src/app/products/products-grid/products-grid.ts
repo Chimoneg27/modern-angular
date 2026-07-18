b47019fc9c028d9fbfa679b0dd7052026025aba5
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-18 03:13:01 +0200

    add search feature

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -1,15 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { ProductCard } from '../product-card/product-card';
 import { Product } from '../product';
-import { MatIconModule } from "@angular/material/icon";
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormField, MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductCard, MatIconModule],
+  imports: [
+    FormsModule,
+    ProductCard,
+    MatIconModule,
+    MatInputModule,
+    MatFormField,
+    MatFormFieldModule,
+  ],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.css',
 })
 export class ProductsGrid {
+  protected readonly searchTerm = signal(''); // a writeable
+
   protected readonly products = signal<Product[]>([
     {
       id: 1,
@@ -34,4 +46,12 @@ export class ProductsGrid {
       originalPrice: 99.99,
     },
   ]);
+
+  protected clearSearch(): void {
+    this.searchTerm.set('');
+  }
+
+  protected trimSearch(): void {
+    this.searchTerm.update((value) => value.trim());
+  }
 }
